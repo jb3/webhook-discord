4d009518b2530fa4c2db8740a59e31ea9b96b149
Joseph <joebean13@outlook.com>
2016-11-19 15:16:20 +0000

    K

--- tests/travis.js
+++ tests/travis.js
@@ -1,6 +1,8 @@
 var WebhookCreator = require("../")
 var Hook = new WebhookCreator(process.env.WEBHOOK_URL)
 
+setTimeout(function(){
+
 Hook.success("Travis","Testing all message types")
 Hook.error("Travis","Testing all message types")
 Hook.info("Travis","Testing all message types")
@@ -8,3 +10,4 @@ Hook.warning("Travis","Testing all message types")
 Hook.custom("Travis","Testing all message types","Travis","#ff00b2")
 
 process.exit(0)
+},5000)
